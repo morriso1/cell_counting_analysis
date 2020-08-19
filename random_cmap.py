bbe246d9870ccf5314834b0651ee3eccd8cc4246
Otto Morris <ottomorris@gmail.com>
2020-08-19 09:01:20 -0700

    added random_cmap function from Guillaume Witz

---
+++ random_cmap.py
@@ -0,0 +1,17 @@
+# function created by Guillaume Witz
+
+import matplotlib
+
+
+def random_cmap():
+    np.random.seed(42)
+    cmap = matplotlib.colors.ListedColormap(np.random.rand(256, 4))
+    # value 0 should just be transparent
+    cmap.colors[:, 3] = 0.5
+    cmap.colors[0, :] = 1
+    cmap.colors[0, 3] = 0
+
+    # if image is a mask, color (last value) should be red
+    cmap.colors[-1, 0] = 1
+    cmap.colors[-1, 1:3] = 0
+    return cmap
